e773ee191e7e2a76e2cc71e141d4c4c7996845de
s u m a n t h <badampraveena1981@gmail.com>
2023-08-26 12:34:28 +0530

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,27 @@
-import { BrowserRouter } from 'react-router-dom'
-import { About, Contact, Experience, Hero, Navbar, Works, StarsCanvas, Tech } from './components'
+import { BrowserRouter } from "react-router-dom";
+
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 
 const App = () => {
-
   return (
     <BrowserRouter>
-      <div className="relative z-0 bg-primary">
-        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+      <div className='relative z-0 bg-primary'>
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
           <Navbar />
           <Hero />
-
         </div>
         <About />
         <Experience />
         <Tech />
         <Works />
         
-
+        <div className='relative z-0'>
+          <Contact />
+          <StarsCanvas />
+        </div>
       </div>
-
-      <div className="relative z-0 ">
-        <Contact />
-        <StarsCanvas />
-      </div>
-
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -8,61 +8,103 @@ import { SectionWrapper } from "../hoc";
 import { projects } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
 
+const ProjectCard = ({
+  index,
+  name,
+  description,
+  tags,
+  image,
+  source_code_link,
+}) => {
+  return (
+    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+      <Tilt
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full'
+      >
+        <div className='relative w-full h-[230px]'>
+          <img
+            src={image}
+            alt='project_image'
+            className='w-full h-full object-cover rounded-2xl'
+          />
 
-
-const ProjectCard = ({ index,name,description,tags,image,source_code_Link}) => (
-  <motion.div variants={fadeIn("up","spring",index*0.5,0.75)} className="w-full md:w-1/2 lg:w-1/3">
-    <Tilt className="bg-tertiary p5 rounded-2xl sm:w-[360px] w-full" options={{ max: 25  ,scale:1, speed:450}}>
-      <div className="relative w-full h-[230px]">
-        <img src={image} alt={name} className="w-full h-full object-cover rounded-2xl" />
-        <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
-          <div className="flex">
-          <div onClick={()=>window.open(source_code_Link,"_blank")} className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer">
-  <img src={github} alt="github" className="w-1/2 h-1/2 object-contain" />
+          <div className='absolute inset-0 flex justify-end m-3 card-img_hover'>
+            <div
+              onClick={() => window.open(source_code_link, "_blank")}
+              className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'
+            >
+              <img
+                src={github}
+                alt='source code'
+                className='w-1/2 h-1/2 object-contain'
+              />
+            </div>
+            <div
+              onClick={() => window.open(source_code_link, "_blank")}
+              className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'
+            >
+              <img
+                src={github}
+                alt='source code'
+                className='w-1/2 h-1/2 object-contain'
+              />
+            </div>
           </div>
-
         </div>
-        <div onClick={()=>window.open(source_code_Link,"_blank")} className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer">
-  <img src={github} alt="github" className="w-1/2 h-1/2 object-contain" />
-          </div>
-          </div>
-      </div>
-      <div className="mt-5 ">
-        <h3 className="text-white font-bold text-[24px]">{name}</h3>
-        <p className="mt-2 text-secondary text-[14px]">{description}</p>
 
-      </div>
-      <div className="mt-4 flex flex-wrap gap-2">
-        {tags.map((tag) => (
-          <p key={tag.name} className={`text-[14px] ${tag.color} px-2 py-1 rounded-md`}>#{tag.name}</p>
-        ))}
-      </div>
-    </Tilt>
-  </motion.div>
+        <div className='mt-5'>
+          <h3 className='text-white font-bold text-[24px]'>{name}</h3>
+          <p className='mt-2 text-secondary text-[14px]'>{description}</p>
+        </div>
 
-)
+        <div className='mt-4 flex flex-wrap gap-2'>
+          {tags.map((tag) => (
+            <p
+              key={`${name}-${tag.name}`}
+              className={`text-[14px] ${tag.color}`}
+            >
+              #{tag.name}
+            </p>
+          ))}
+        </div>
+      </Tilt>
+    </motion.div>
+  );
+};
+
 const Works = () => {
   return (
     <>
-    <motion.div variants={textVariant()}>
-    <p className={styles.sectionSubText}>My work</p>
-        <h2 className={styles.sectionHeadText}>Projects.</h2>
-    </motion.div>
-    <div className="w-full flex">
-      <motion.p variants={fadeIn("","",0.1,1)} className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]">
-        I have worked on a variety of projects, ranging from web development to
-        machine learning. Here are a few of them.It reflects my ability to solve complex problems,work with different technologies,and manage
-        projects effectively.
+      <motion.div variants={textVariant()}>
+        <p className={`${styles.sectionSubText} `}>My work</p>
+        <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
+      </motion.div>
 
-      </motion.p>
-    </div>
-    <div className="mt-20 flex flex-wrap gap-7">
-      {projects.map((project, index) => (
-        <ProjectCard key={`project-${index}`} index={index} {...project}/>
-      ))}
-    </div>
+      <div className='w-full flex'>
+        <motion.p
+          variants={fadeIn("", "", 0.1, 1)}
+          className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
+        >
+          Following projects showcases my skills and experience through
+          real-world examples of my work. Each project is briefly described with
+          links to code repositories and live demos in it. It reflects my
+          ability to solve complex problems, work with different technologies,
+          and manage projects effectively.
+        </motion.p>
+      </div>
+
+      <div className='mt-20 flex flex-wrap gap-7'>
+        {projects.map((project, index) => (
+          <ProjectCard key={`project-${index}`} index={index} {...project} />
+        ))}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default SectionWrapper(Works,"");
+export default SectionWrapper(Works, "");
